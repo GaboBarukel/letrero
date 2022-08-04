e8d4f2f72b8f8238ddfeb4514564a45470724213
Santiago Gesualdo <santigesualdo@gmail.com>
2022-08-04 12:39:33 -0300

    refactoring Timer class

--- js/timer.js
+++ js/timer.js
@@ -1,34 +1,38 @@
-function Timer(callback, timeInterval, errorCallback) {
-  this.timeInterval = timeInterval;
+class Timer{
+  constructor(callback, timeInterval, errorCallback){
+    this.callback = callback
+    this.timeInterval = timeInterval
+    this.errorCallback = errorCallback
+  }
 
   //Start timer
-  this.start = () => {
+  start = () => {
     this.expected = Date.now() + this.timeInterval;
     this.timeout = setTimeout(this.round, this.timeInterval);
     console.log("timer started");
-  };
+  }
+
   //Stop timer
-  this.stop = () => {
+  stop = () => {
     clearTimeout(this.timeout);
     console.log("timer stopped");
-  };
+  }
+
   //Method that take cares of running our callback and adjusting the time interval
-  this.round = () => {
+  round = () => {
     let drift = Date.now() - this.expected;
 
     //Changing tabs error handling
     if (drift > this.timeInterval) {
       if (errorCallback) {
-        errorCallback();
+        this.errorCallback();
       }
     }
 
-    callback();
+    this.callback();
     this.expected += this.timeInterval;
-    // console.log(drift);
-    // console.log(this.timeInterval - drift);
     this.timeout = setTimeout(this.round, this.timeInterval - drift);
-  };
+  }
 }
 
 export default Timer;
